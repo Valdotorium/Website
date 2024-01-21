dbb8284ce040cedf2adfe5f084645379820b28fe
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-01-21 13:35:40 +0100

    first js

---
+++ res/sites/scripts/project_box.js
@@ -0,0 +1,8 @@
+//setting content of project boxes
+
+function SetContent(){
+    var box = document.getElementById("ProjectBox");
+    var para = document.createElement("p");
+    para.innerText = "jhqgvde"
+    box.appendChild(para);
+}
