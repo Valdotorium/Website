ce3ee27a955c1a17cb3993d432cee010a007f42b
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-01-25 20:11:43 +0100

    some AI python

--- res/sites/scripts/project_box.py
+++ res/sites/scripts/project_box.py
@@ -1,1 +1,20 @@
 from pyscript import document
+import json
+
+#fix imports
+#TODO:
+#watch some pyscript tutorial
+
+def read_json(json_file):
+    with open(json_file, 'r') as f:
+        return json.load(f)
+
+json = read_json("json.json")
+
+#work with the json object
+
+print(json)
+
+#put the json element in a html paragraph
+#maybe that?:
+document.add_paragraph(json)
