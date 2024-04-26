81eaf17cdf0ece94b5f81592707b94175d959bec
Valdotorium <valdotorium@icloud.com>
2024-04-26 15:09:47 +0200

    prep for 2nd projects, page needs rebuild

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ window.onload=function(){
     ctx.font = "8px Ubuntu"
     let CurrentProject = 0
     let Projects = [
-        {"Title": "PyKart Drive","Image": "Project1Image","Description": "This is a game written in Python with pygame. \n  The player can build their own vehicles and the drive them in  \n procedurally generated landscapes the player can select. \n the building system is not based on tiles, \n and there is a total of 27 parts organized in categories to build with \n The driving of the vehicle features particles, a minimap and physics made possible by pymunk. \n See <a href=https://www.reddit.com/r/pygame/comments/1bu9d0b/my_pygame/> This reddit post </a> for gameplay footage. \n <a href=https://github.com/Valdotorium/Project-Exoplanet/> here </a> is the GitHub repository of the project.",}]
+        {"Title": "PyKart Drive","Image": "Project1Image","Description": "This is a game written in Python with pygame. \n  The player can build their own vehicles and the drive them in  \n procedurally generated landscapes the player can select. \n the building system is not based on tiles, \n and there is a total of 27 parts organized in categories to build with \n The driving of the vehicle features particles, a minimap and physics made possible by pymunk. \n See <a href=https://www.reddit.com/r/pygame/comments/1bu9d0b/my_pygame/> This reddit post </a> for gameplay footage. \n <a href=https://github.com/Valdotorium/Project-Exoplanet/> here </a> is the GitHub repository of the project.",}
+        {"Title": "VGraphs","Image": "Project1Image", "Description": "A website which can be used to animate data in "}]
 
     function draw(){
         let DrawProject = Projects[CurrentProject]
